123eb0f3bc53873f5b3a5a453f2e16a6d4910332
Jaeseok Park <m2363@naver.com>
2022-07-19 18:24:32 +0900

    sale page

--- sale/urls.py
+++ sale/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ArtListView.as_view(), name='art'),
-    path('sale/<int:pk>/', views.CreateView.as_view(), name='sale'),
+    path('sale/<int:pk>/', views.SaleView.as_view(), name='sale'),
     # path('bills/', views.salebillview, name='bills'),
 ]
--- sale/views.py
+++ sale/views.py
@@ -13,32 +13,30 @@ class ArtListView(FilterView):
     paginate_by = 10
 
 
-class BillCreateView(CreateView):
-    # 페이지 로드하면서 상세페이지 + 수정페이지
-    # GET 요청 시 상세페이지 + 입력칸
-    # 지갑 주소, 영수증 번호 입력 후 저장 버튼 누르면
-    # 구매 목록에 작품ID + 구매 내역 save()
+class SaleView(View):
+    template_name = 'sale_page.html'
 
-    model = SaleBill
-    template_name = 'sale.html'
-    form_class = SaleForm
+    def get(self, request, pk):
+        form = SaleForm(request.GET or None)
+        item = Art.objects.filter(pk=pk)
+        context = {
+            'form': form,
+            'item': item,
+        }
+        return render(request, self.template_name, context)
 
-#     def get(self, request):
-#         model = Art
-#         form = SaleForm(request.GET or None)
-#         arts = Art.objects.filter()
-#         pass
-#     def post(self, request):
-#         pass
-#
-#
-# def detailview(request, pk):
-#     if request.method == 'GET':
-#         model = Art
-#
-#     else:
-#     pass
-#
-#
-# def salebillview(request):
-#     pass
+    def post(self, requset):
+        form = SaleForm(requset.POST)
+        if form.is_valid():
+            sale_bill = form.save(commit=False)
+            sale_bill.save()
+
+            bill = SaleBill(id=id)
+            bill.save()
+
+
+
+        context = {
+
+        }
+        return render(requset, self.template_name, context)
